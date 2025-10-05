0714bd3981d94fe8334020486e02d197d1021e99
Alex Candela <alex.candelaa@gmail.com>
2025-10-05 16:30:55 +0200

    Bloque stock reciente casi acabado, (posibles cambios)

--- stockfastfrontend/src/app/pages/general-component/tips-component/tips-component.ts
+++ stockfastfrontend/src/app/pages/general-component/tips-component/tips-component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartComponent } from 'ng-apexcharts';
+
+import {
+  NgApexchartsModule,
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+  dataLabels: ApexDataLabels;
+  legend: ApexLegend;
+};
 
 @Component({
   selector: 'app-tips-component',
-  imports: [],
+  imports: [NgApexchartsModule],
   templateUrl: './tips-component.html',
-  styleUrl: './tips-component.scss'
+  styleUrl: './tips-component.scss',
 })
 export class TipsComponent {
+  @ViewChild('chart') chart!: ChartComponent;
+  public chartOptions: ChartOptions;
 
+  constructor() {
+    this.chartOptions = {
+      series: [132, 56, 389],
+      chart: {
+        height: '100%',
+        type: 'pie',
+        
+      },
+      labels: ['Productos', 'Envío', 'Beneficio'],
+      dataLabels: {
+        enabled: true,
+      },
+      legend: {
+        show: false,
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: 'bottom',
+            },
+          },
+        },
+      ],
+    };
+  }
 }
