ed2d231827d0d75cc170dab48d54b0a563d3b421
alexcandela <alex.candelaa@gmail.com>
2025-10-06 20:49:28 +0200

    Inicio pagina stock

--- stockfastfrontend/src/app/pages/stock-component/stock-component.ts
+++ stockfastfrontend/src/app/pages/stock-component/stock-component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { BuscadorComponent } from '../../components/buscador-component/buscador-component';
 @Component({
   selector: 'app-stock-component',
-  imports: [],
+  imports: [BuscadorComponent],
   templateUrl: './stock-component.html',
   styleUrl: './stock-component.scss'
 })
 export class StockComponent {
-
+  
 }
