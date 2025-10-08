248017b9655e17c3274e32a225a23864f2300bd5
Alex Candela <alex.candelaa@gmail.com>
2025-10-08 20:44:32 +0200

    Diseño pagina stock completada

---
+++ stockfastfrontend/src/app/components/productlist-component/productlist-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productlist-component',
+  imports: [],
+  templateUrl: './productlist-component.html',
+  styleUrl: './productlist-component.scss'
+})
+export class ProductlistComponent {
+
+}
--- stockfastfrontend/src/app/pages/stock-component/stock-component.ts
+++ stockfastfrontend/src/app/pages/stock-component/stock-component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { BuscadorComponent } from '../../components/buscador-component/buscador-component';
+import { ProductlistComponent } from '../../components/productlist-component/productlist-component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-stock-component',
-  imports: [BuscadorComponent],
+  imports: [BuscadorComponent, ProductlistComponent, CommonModule],
   templateUrl: './stock-component.html',
   styleUrl: './stock-component.scss'
 })
