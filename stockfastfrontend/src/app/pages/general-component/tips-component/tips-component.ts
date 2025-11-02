19022b3a8488d7eeee6eb974644b9275fe55fa17
Alex Candela <alex.candelaa@gmail.com>
2025-11-02 14:58:13 +0100

    Mejorar la presentación de datos en el componente de tips: ajustar estilos para números grandes y agregar lógica para determinar si un número es grande.

--- stockfastfrontend/src/app/pages/general-component/tips-component/tips-component.ts
+++ stockfastfrontend/src/app/pages/general-component/tips-component/tips-component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { StockService } from '../../../core/services/stock-service';
 import { Stock, StockResponse } from '../../../core/interfaces/stock';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tips-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './tips-component.html',
   styleUrl: './tips-component.scss',
 })
@@ -12,7 +14,19 @@ export class TipsComponent {
   @Input() stockTotal: number | null = null;
   @Input() stockData: Stock | null = null;
 
-  constructor() {};
+  constructor() {}
 
-  
+  isLargeNumber(value: number | undefined): boolean {
+    if (value == null) return false; // Verifica undefined y null
+    const numberString = value.toFixed(2);
+    return numberString.length > 7;
+  }
+
+  isStockLargeNumber(value: number | null | undefined): boolean {
+    if (value == null) return false; // Verifica undefined y null
+    const numberString = value.toString();
+    console.log(numberString);
+    
+    return numberString.length > 3;
+  }
 }
